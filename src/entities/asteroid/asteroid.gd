0c3c9a2a83839769447164341b6b8955abafc8ef
Zircon <zircon332@gmail.com>
2023-02-05 22:18:15 +0800

    implemented asteroid die

--- src/entities/asteroid/asteroid.gd
+++ src/entities/asteroid/asteroid.gd
@@ -5,13 +5,17 @@ var dir := Vector2.ZERO
 export var speed := 100
 
 
-func _ready():
+func _ready() -> void:
 	spawn()
 
 
-func spawn():
+func spawn() -> void:
 	dir = Vector2(randf(), randf())
 
 
 func _physics_process(delta) -> void:
 	position += delta * speed * dir
+
+
+func hurt() -> void:
+	queue_free()
